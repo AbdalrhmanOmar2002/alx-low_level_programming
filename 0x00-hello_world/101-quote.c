390e5c8bf81bcc2cc2f2a76f1b6362460b8a3787
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-05 18:49:38 -0700

    add

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that prints with printf function.
+ *	write(int fd,const void *buf,size_t count)
+ *
+ * Return: Always 1 (not success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
+}
