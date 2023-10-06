844d75a6ceb6ee6b41fd2592f4516423ded0d301
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-05 17:57:11 -0700

    add

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: A C program that Prints with put function
+ *
+ * Return : Alawys 0 (Success)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
