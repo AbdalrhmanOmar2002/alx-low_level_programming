2586998b10e9d9bc7f6250097a6d37db847e00db
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-05 18:54:14 -0700

    add

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,14 +4,13 @@
  * main - Entry Point
  *
  * Description: A C program that prints with printf function.
- *	write(int fd,const void *buf,size_t count)
+ *	ssize_t write(int fd, const void *buf, size_t count)
  *
- * Return: Always 1 (not success)
+ * Return: 1 (error)
 */
 
 int main(void)
-{
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{		char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(1, quo, 59);
 	return (1);
