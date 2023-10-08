89ba751875a97b2dd5be1836385316dbf0d04bc3
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-08 16:40:16 -0700

    more headers goes there b

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ **/
+
+int main(void)
+{
+char n = 'z';
+for (; n <= 'a'; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
