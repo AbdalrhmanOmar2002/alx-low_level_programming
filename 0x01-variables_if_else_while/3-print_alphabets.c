48a3ffdbf0c18d29d97f510ae699ef7240df5d42
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-08 15:34:49 -0700

    more headers goes there b

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+**/
+
+int main(void)
+{
+char ch = 'a';
+char ch2 = 'A'
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+while (ch2 <= 'Z')
+{
+putchar(ch2);
+ch2++;
+}
+
+putchar('\n');
+return (0);
+}
