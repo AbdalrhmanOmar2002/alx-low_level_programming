b0562da66ebfdad19816c790d0b3da3bbfa006b0
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-08 16:56:08 -0700

    more headers goes there b

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+ **/
+
+int main(void)
+{
+int n = 48;
+for (; n <= 57; n++)
+{
+putchar(n);
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
