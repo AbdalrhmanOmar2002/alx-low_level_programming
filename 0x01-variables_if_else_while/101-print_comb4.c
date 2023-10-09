53d4696d29676263de92a7c3972851ae82781a80
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-08 17:36:16 -0700

    more headers goes there b

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -26,7 +26,7 @@ if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
 putchar(digit1 + 48);
 putchar(digit2 + 48);
 putchar(digit3 + 48);
-if (digit1 + digit2 + digit3 != 29)
+if (digit1 + digit2 + digit3 != 24)
 {
 putchar(',');
 putchar(' ');
