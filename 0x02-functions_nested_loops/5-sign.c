6857fd0e6c396f0b5b0c2bc9179eb4084b5e55ac
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-10 16:47:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 {
 if (n > 0)
 {
-_putcher(43);
+_putchar(43);
 return (1);
 }
 else if (n == 0)
 {
-_putcher(48);
+_putchar(48);
 return (0);
 }
 else
 {
-_putcher(45);
+_putchar(45);
 return (-1);
 }
 }
