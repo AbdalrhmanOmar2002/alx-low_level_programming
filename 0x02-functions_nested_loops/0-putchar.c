b8bf4cf77f90c89562b7f990a2c432bb1c4e5d56
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-10 14:58:39 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: 0 (success)
+**/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
