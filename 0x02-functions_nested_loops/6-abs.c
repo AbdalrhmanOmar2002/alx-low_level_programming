02cc4f34a2b9fed6618b4ce30c628919cda82e41
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-10 17:00:56 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - that computes the absolute value of an integer.
+ *
+ * @j: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int j)
+{
+if (j > 0)
+return (j);
+if (j < 0)
+return (-j);
+}
