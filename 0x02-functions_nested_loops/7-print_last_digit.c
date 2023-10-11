96e00284b53a437e43c5b0065c9fe030fd04c169
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-10 17:08:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - that computes the absolute value of an integer.
+ *
+ * @j: takes in integer type input for function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int j)
+{
+int i = j % 10;
+return (i);
+}
