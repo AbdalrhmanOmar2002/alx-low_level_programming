1835efdd64ad5f5d9a2284db09829374ed957582
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-10 17:13:12 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,13 +3,21 @@
 /**
  * print_last_digit - that computes the absolute value of an integer.
  *
- * @j: takes in integer type input for function
+ * @n: takes in integer type input for function
  *
  * Return: Always 0 (Success)
  */
 
-int print_last_digit(int j)
+int print_last_digit(int n)
 {
-int i = j % 10;
+int i;
+if (n < 0)
+{
+i = -1 * (n % 10)
+}else
+{
+i = n % 10
+}
+_putchar(i + '0')
 return (i);
 }
