0bcebdb84d1c13cf968e23dc774ec3a93999bf40
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-12 15:00:13 -0700

    0x03-debugging

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9
+ *Do not print 2 and 4
+ *only using _putchar twice
+ *
+ * Return : Always O (Success)
+ */
+void print_most_numbers(void)
+{
+char i = 48;
+while (i <= 57)
+{
+if (i == 50 || i == 52)
+continue;
+else
+_putchar(i);
+i++;
+}
+_putchar('\n');
+}
