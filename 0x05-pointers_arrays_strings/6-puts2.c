5310dd354310a8f2105fbbc5b8faf147b4c15cf0
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-16 17:38:23 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts2 - reverses a string.
+ *
+ * @str: a pointer to the string
+ *
+ * Return: Nothing
+ */
+
+void puts2(char *str)
+{
+int num, i;
+for (num = 0; str[num] != '\0'; num++)
+{
+;
+}
+for (i = 0; i < num; i++)
+{
+if (str[i] % 2 != 0)
+{
+;
+}
+else
+{
+_putchar(str[i]);
+}
+}
+}
