860cec23993307069fcba43dd1880aaed9a79e31
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-16 18:08:45 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,25 +11,22 @@
 void puts_half(char *str)
 {
 int i, l;
-int num;
+int all;
 for (i = 0; str[i] != '\0'; i++)
 {
 ;
 }
-
 l = i / 2;
+for (all = l; all < i; all++)
+{
 if (i % 2 == 0)
 {
-for (num = l; num < i; num++)
-{
-printf("%d", num);
-}
+_putchar(str[all]);
 }
 else
 {
-for (num = l; num <= i; num++)
-{
-printf("%d", num);
+_putchar(str[all]);
 }
 }
+_putchar('\n');
 }
