d9175a97dabdd36ee87ba598c09a50076e341718
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-16 20:59:56 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,9 +11,7 @@ int _atoi(char *s)
 {
 unsigned int num = 0;
 int sign = 1;
-
-do
-{
+do {
 if (*s == '-')
 {
 sign *= -1;
@@ -27,6 +25,5 @@ else if (num > 0)
 break;
 }
 } while (*s++);
-
-return (num * sign);
+return (num *sign);
 }
