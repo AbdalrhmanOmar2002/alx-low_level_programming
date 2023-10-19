e9780c2df4a2a63044c9f906d52f52f5eb12f699
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-19 07:56:00 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -15,7 +15,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int i = 0, j = 0, k, l = 0, f, s, d = 0;
 
-/* Get the lengths of the two strings*/
 while (n1[i] != '\0')
 i++;
 while (n2[j] != '\0')
@@ -23,11 +22,10 @@ j++;
 
 l = (i > j) ? i : j;
 
-/* Check if the resulting string can fit into the provided buffer*/
 if (l + 2 > size_r)
-return (0); /* We add 2: one for potential carry and one for the null terminator*/
+return (0);
 
-r[l + 1] = '\0'; /* Place the null terminator at the end*/
+r[l + 1] = '\0';
 
 for (k = l; k > 0; k--)
 {
@@ -42,9 +40,8 @@ d = (f + s + d) / 10;
 
 r[0] = d + '0';
 
-/* If there's no carry in the most significant position*/
 if (r[0] == '0')
 return (r + 1);
 
-return r;
+return (r);
 }
