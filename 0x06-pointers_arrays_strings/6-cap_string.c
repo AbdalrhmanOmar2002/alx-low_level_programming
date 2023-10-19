0780900a810174c5cbbae83b452ee4aac1c675b5
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-10-18 17:58:37 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: char pointer
+ *
+ * Return: Nothing
+ */
+
+char *cap_string(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] == 32 || str[i] == '\n')
+{
+i++;
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+}
+if (str[i] == '\t')
+{
+str[i] = 32;
+}
+}
+return (str);
+}
