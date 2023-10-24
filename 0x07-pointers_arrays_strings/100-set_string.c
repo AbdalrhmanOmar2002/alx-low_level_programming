3d79e0810b90bbfb402bf1c1b19350111a242cf3
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-10-24 05:43:22 +0300

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints the sum of the two diagonals
+ * of a square matrix of integers.
+ * @s: pointer to pointer int
+ * @to: pointer to char
+ *
+ * Return: Nothing.
+ */
+
+void set_string(char **s, char *to)
+{
+  *s = to;
+}
