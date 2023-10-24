b29e76bd56dfeb972383bc889d57c89b8ef0a0b0
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-10-24 03:33:50 +0300

    0

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * char_length - length of array.
- * @a: char array
- *
- * Return: length of array..
- */
-int char_length(char a[])
-{
-int i = 0;
-while (a[i] != '\0')
-{
-i++;
-}
-
-return (i);
-
-}
-
-/**
  * _strchr - locates a character in a string.
  * @s: char pointer
  * @c: char
@@ -29,9 +11,8 @@ return (i);
 char *_strchr(char *s, char c)
 {
 int i;
-int n = char_length(s);
 
-for (i = 0; i < n; i++)
+for (i = 0; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
 return (s + i);
