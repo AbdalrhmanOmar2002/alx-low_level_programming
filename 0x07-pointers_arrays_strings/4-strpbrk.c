6dfc6d37246bdb54fded6beffa3e4b0a074a862e
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-10-24 04:14:55 +0300

    0

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,17 +11,18 @@
 char *_strpbrk(char *s, char *accept)
 {
 
-  int i, g;
-  for (i = 0; s[i] != '\0'; i++)
-  {
-    for (g = 0; accept[g] != '\0'; g++)
-    {
-      if (accept[g] == s[i])
-      {
-        return (s + i);
-      }
-    }
-  }
+int i, g;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (g = 0; accept[g] != '\0'; g++)
+{
+if (accept[g] == s[i])
+{
+return (s + i);
+}
+}
+}
 
-  return ('\0');
+return ('\0');
+
 }
