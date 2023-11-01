3fccabb77b4530ad698a6193cf20a6182350f9e6
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-01 20:16:47 +0200

    0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,12 +11,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-  char *n = malloc(size);
-  if (size == 0 || n == 0)
-    return (0);
-  while (size--)
-  {
-    n[size] = c;
-  }
-  return (n);
+char *n = malloc(size);
+if (size == 0 || n == 0)
+return (0);
+while (size--)
+{
+n[size] = c;
+}
+return (n);
 }
