4a2aa446dd6d692c678e29509de43a629a344048
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-07 02:20:13 +0200

    bog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct my_struct - Short description
+ * @*name: First member
+ * @*owner: Second member
+ * @age: Third member
+ *
+ * Description: Longer description
+ */
+
 struct dog
 {
   char *name;
