3a13da1fcaf32b7f3d1dc74d48e5f527b79fb218
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-09 04:00:28 +0200

    simple operations.

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - add tow integers
+ * @a: first int
+ * @b: secund int
+ *
+ * Return: the sum
+ */
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - sub tow integers
+ * @a: first int
+ * @b: secund int
+ *
+ * Return: the sub
+ */
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - mul tow integers
+ * @a: first int
+ * @b: secund int
+ *
+ * Return: the mul
+ */
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - div tow integers
+ * @a: first int
+ * @b: secund int
+ *
+ * Return: the div
+ */
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - mod tow integers
+ * @a: first int
+ * @b: secund int
+ *
+ * Return: the mod
+ */
+int op_mod(int a, int b)
+{
+return (a % b);
+}
