93937e694381c29afc396a5e712501ee05e5d485
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-10 02:29:02 +0200

    0

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
 
 /**
  * print_strings - prints string, followed by a new line.
@@ -19,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < (int)n; i++)
 	{
 		str = va_arg(arr, char *);
-		
+
 		if (str)
 			printf("%s", str);
 		else
