60ba284aef4770f76850a84010cc630ac886a83a
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-10 03:34:42 +0200

    0

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -42,8 +42,11 @@ void format_string(char *separator, va_list ap)
 	char *str = va_arg(ap, char *);
 
 	switch ((int)(!str))
+	{
 	case 1:
 		str = "(nil)";
+	}
+	
 	printf("%s%s", separator, str);
 }
 
