67dc88b4ddef9abefabc59972d5a56891c6f1a19
Abdalrhman Omar <90539497+AbdalrhmanOmar2002@users.noreply.github.com>
2023-11-10 02:22:48 +0200

    0

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,40 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints string, followed by a new line.
+ * @n: number of separators
+ * @separator: separator
+ * Abdalrhman Omar
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list arr;
+	int i;
+	char *str;
+
+	va_start(arr, n);
+
+	for (i = 0; i < (int)n; i++)
+	{
+		str = va_arg(arr, char *);
+		
+		if (str)
+		{
+			printf("%s", str);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		if (separator && i < (int)n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+
+	va_end(arr);
+}
