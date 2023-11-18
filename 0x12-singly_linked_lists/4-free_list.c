ad6d6eb9fbf17c420c9d577f903fd6912cbada9d
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-18 02:17:06 +0200

    back

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,5 +8,18 @@
 
 void free_list(list_t *head)
 {
-	free(head);
+	list_t *node, *nextNode;
+
+	if (!head)
+		return;
+
+	node = head;
+
+	while (node)
+	{
+		nextNode = node->next;
+		free(node->str);
+		free(node);
+		node = nextNode;
+	}
 }
