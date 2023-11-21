69534682367a84d575f9f3ea0e6a80bb0ceb8241
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-22 01:12:21 +0200

    edit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -3,20 +3,23 @@
 /**
  * get_nodeint_at_index - Get node at index
  * @head: pointer to struct node
+ * @index: unsigned int
  *
- * Return:  the nth node of a listint_t linked list
+ * Return: the nth node of a listint_t linked list
  */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *newNode = head;
+
 	if (!head)
 		return (NULL);
 
 	while (index)
 	{
-		head = head->next;
+		newNode = newNode->next;
 		index--;
 	}
 
-	return (head);
+	return (newNode);
 }
