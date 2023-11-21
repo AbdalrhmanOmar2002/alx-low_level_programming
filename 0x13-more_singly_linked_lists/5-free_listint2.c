81ef5b80c255f64c8118f157cc8275330c74aa24
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-21 15:52:57 +0200

    some edit

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,9 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a listint_t list
+ * free_listint2 - frees a listint_t list.
  * @head: pointer to struct node
  *
- * Return: the address of the new element, or NULL if it failed
  */
 
 void free_listint2(listint_t **head)
@@ -12,9 +11,7 @@ void free_listint2(listint_t **head)
 	listint_t *newNode, *temp;
 
 	if (!head)
-	{
 		return;
-	}
 
 	newNode = *head;
 
