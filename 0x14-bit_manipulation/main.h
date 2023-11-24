118c9009f5daa702ca4236dd1a9d93e566569d52
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-25 01:52:16 +0200

    get_endianness

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,10 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <limits.h>
 
-#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -14,4 +12,5 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 #endif
