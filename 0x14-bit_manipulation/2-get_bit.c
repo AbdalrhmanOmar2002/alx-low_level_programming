13086b6aa11b980ffed3ef6ffc2cccb7b95e4de9
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-24 16:32:38 +0200

    get_bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * get_bit - the value of a bit at a given index.
+ * @n: unsigned long int
+ * @index: unsigned int
+ *
+ * Return: the value of the bit at index index or -1 if an error occurred
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit = sizeof(n) * 8;
+	char *arr = malloc(bit * sizeof(char));
+	int i = 0;
+
+	while (n > 0)
+	{
+		arr[i] = (n % 2 == 0) ? '0' : '1';
+		n = n / 2;
+		i++;
+	}
+
+	return (arr[index] - '0');
+}
