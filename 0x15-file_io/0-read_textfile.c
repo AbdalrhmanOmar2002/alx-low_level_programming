0e390112e6f0f6495f025189ddbc35c1d33ebc0d
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-28 01:13:09 +0200

    read_textfile

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * read_textfile - prints the binary representation of a number.
+ * @filename: file name to reade
+ * @letters: number of bytes to read
+ *
+ * Return: actual number of letters it could read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fp;
+	ssize_t bytes;
+	char buf[READ_BUFFER_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+
+	fp = open(filename, O_RDONLY);
+	if (fp < 0)
+		return (-1);
+
+	bytes = read(fp, &buf[0], letters);
+	bytes = write(1, &buf[0], bytes);
+	close(fp);
+	return (bytes);
+}
