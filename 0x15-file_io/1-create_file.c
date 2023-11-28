fe5a98b869a757339413e09cfd0784bfc17492af
AbdalrhmanOmar2002 <witid2002@gmail.com>
2023-11-28 02:00:25 +0200

    create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string.
+ *
+ * @s: a pointer to the string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (!s)
+		return (0);
+	while (*s++)
+		i++;
+
+	return (i);
+}
+
+/**
+ * create_file - creates a file.
+ * @filename: file name to reade
+ * @text_content: thr terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fp;
+	ssize_t bytes;
+	ssize_t len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+
+	fp = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (fp < 0)
+		return (-1);
+
+	if (len)
+		bytes = write(fp, text_content, len);
+	close(fp);
+	return (bytes == len ? 1 : -1);
+}
